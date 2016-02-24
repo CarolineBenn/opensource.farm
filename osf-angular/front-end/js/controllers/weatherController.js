0c0763f45ea69668ba6f57a9e0c73ab5f2a7aa9b
CarolineBenn <lineybenn@gmail.com>
2016-02-24 23:03:11 +0000

    Got the link-to-project working

--- osf-angular/front-end/js/controllers/weatherController.js
+++ osf-angular/front-end/js/controllers/weatherController.js
@@ -5,14 +5,16 @@ angular
   WeatherController.$inject = ["$http"];
   function WeatherController($http){
     var self = this;
-    self.weather = {};
+
 
   function getWeather(){
     $http
     .get('http://datapoint.metoffice.gov.uk/public/data/val/wxfcs/all/json/322494?res=3hourly&?key=e652296c-758b-458f-a584-d2360ae6606a')
     .then(function(res){
-      console.log(res);
-
+      console.log(res.data)
+      self.weather = res.data.SiteRep.DV.Location.Period[0].Rep[0]   
+      self.updated = res.data.SiteRep.DV.Location.Period[0].value;   
+      console.log(res.data.SiteRep.DV.Location.Period[0].Rep[0]);
     });
   }
 
