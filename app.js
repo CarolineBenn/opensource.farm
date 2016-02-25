bd0796c2a4ab9a9213c1ef7dfc8ce596cbe3f396
CarolineBenn <lineybenn@gmail.com>
2016-02-25 20:10:03 +0000

    Serving corrent root file v2

--- app.js
+++ app.js
@@ -35,7 +35,7 @@ app.use(cookieParser());
 app.use(morgan('dev'));
 app.use(cors());
 app.use(passport.initialize());
-app.use('/static', express.static(__dirname + '/public'));
+app.use(express.static(__dirname + '/public'));
 app.set('view engine', ejs);
 
 app.use('/api', expressJWT({ secret: secret })
